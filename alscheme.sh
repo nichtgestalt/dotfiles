9e385ba6492516bbcfe0754f31aa2830efe26ff8
nichtgestalt <1993sanderphilipp@gmail.com>
2022-01-18 17:55:27 +0100

    scripts

---
+++ alscheme.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+config="$HOME/.config/alacritty/alacritty.yml"
+
+declare -a options=(
+"nord-dark"
+"breeze"
+"tango"
+"google-dark"
+"codeschool-dark"
+"kasugano"
+"doomicide"
+"nature-suede"
+"muse"
+"jwl-dark"
+"nudge"
+"dracula"
+"cyberpunk"
+)
+
+choice=$(printf '%s\n' "${options[@]}" | dmenu -l 15 -p Schemes:)
+
+case $choice in
+	'nord-dark')
+		sed -i '/^colors:/c\colors: *nord-dark' $config ;;
+	'breeze')
+		sed -i '/^colors:/c\colors: *breeze' $config ;;
+	'tango')
+		sed -i '/^colors:/c\colors: *tango' $config ;;
+	'google-dark')
+		sed -i '/^colors:/c\colors: *google-dark' $config ;;
+	'codeschool-dark')
+		sed -i '/^colors:/c\colors: *codeschool-dark' $config ;;
+	'kasugano')
+		sed -i '/^colors:/c\colors: *kasugano' $config ;;
+	'doomicide')
+		sed -i '/^colors:/c\colors: *doomicide' $config ;;
+	'nature-suede')
+		sed -i '/^colors:/c\colors: *gslob-nature-suede' $config ;;
+	'muse')
+		sed -i '/^colors:/c\colors: *muse' $config ;;
+	'jwl-dark')
+		sed -i '/^colors:/c\colors: *jwl-dark' $config ;;
+	'nudge')
+		sed -i '/^colors:/c\colors: *nudge' $config ;;
+	'dracula')
+		sed -i '/^colors:/c\colors: *dracula' $config ;;
+	'cyberpunk')
+		sed -i '/^colors:/c\colors: *cyberpunk' $config ;;
+esac
---
+++ zshcolors.sh
@@ -0,0 +1,22 @@
+#!/bin/zsh
+
+#color   -   -  lite color
+#black	0   8  black
+#red	1   9  red
+#green	2  10  green
+#yellow	3  11  yellow
+#blue	4  12  blue
+#magenta	5  13  magenta
+#cyan	6  14  cyan
+#white	7  15  white
+
+
+print -P "color   -   -  lite color
+%F{0}black%f	0   8   %F{8}black%f
+%F{1}red%f	1   9   %F{9}red%f
+%F{2}green%f	2   10  %F{10}green%f
+%F{3}yellow%f	3   11  %F{11}yellow%f
+%F{4}blue%f	4   12  %F{12}blue%f
+%F{5}magenta%f	5   13  %F{13}magenta%f
+%F{6}cyan%f	6   14  %F{14}cyan%f
+%F{7}white%f	7   15  %F{15}white%f"
